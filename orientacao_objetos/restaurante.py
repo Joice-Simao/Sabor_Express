7aa2986db6eb8e0c63fd68c7722b6599a3cc215c
Joice-Simao <joice.simao@hotmail.com>
2024-09-25 21:32:30 -0300

    Add Orientação a Objetos

---
+++ orientacao_objetos/restaurante.py
@@ -0,0 +1,14 @@
+class Restaurante:
+    def __init__(self, nome, categoria):
+        self.nome = nome
+        self.categoria = categoria
+        self.ativo = False
+
+restaurante_praca = Restaurante('Praça', 'Gourmet')
+restaurante_pizza = Restaurante('Pizza Express', 'Italiana')
+
+print(vars(restaurante_praca))
+print(vars(restaurante_pizza))
+
+#print(vars(restaurante_praca))
+
